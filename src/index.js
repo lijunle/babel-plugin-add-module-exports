40ed1c1578928f5065746f6bdf0d67ea57d3c905
hdineen <hdineen@hubspot.com>
2019-10-18 12:33:49 -0400

    [New] support multiple assignment of 'export.default' in Babel 7

--- src/index.js
+++ src/index.js
@@ -110,6 +110,7 @@ class ExportsFinder {
     if (objectName === 'exports' || objectName === '_exports') {
       if (propertyName === 'default') {
         this.hasExportsDefault = true
+        this.findExports(path.get(property), 'right')
       } else if (propertyName !== '__esModule') {
         this.hasExportsNamed = true
       }
